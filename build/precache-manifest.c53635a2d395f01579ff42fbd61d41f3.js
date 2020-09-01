8bdaf8667a7ac247a9286d07c6a846dd14608401
Maxym <maksim.parfenov@protonmail.ch>
2020-09-01 11:30:27 +0300

    feat(): build

---
+++ build/precache-manifest.c53635a2d395f01579ff42fbd61d41f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7751b0d9ad78a699997e465aa11802a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f514fa4f5c3fd8df9ae8",
+    "url": "/static/js/2.ac470ffc.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/static/js/2.ac470ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a09fc0ba9f654f26af4",
+    "url": "/static/js/main.fdd1e368.chunk.js"
+  },
+  {
+    "revision": "ea049a9b5aa085bfefe9",
+    "url": "/static/js/runtime-main.fe0cf552.js"
+  }
+]);
